968cbf9b974d799279796d4813a4242130dc693d
Roman Organ <r.organ@dan-it.com.ua>
2023-08-11 18:29:57 +0300

    about, hero, experience sections done

---
+++ src/constants/services.js
@@ -0,0 +1,20 @@
+import { mobile, backend, creator, web } from "../assets";
+
+export const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
